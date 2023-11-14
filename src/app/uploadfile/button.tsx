bd30601cdddfde287136b202fef19325918de1ed
irtizaf <irtizaf@yahoo.com>
2023-11-14 23:15:22 +0500

    back ti default

--- src/app/uploadfile/button.tsx
+++ src/app/uploadfile/button.tsx
@@ -5,96 +5,22 @@ import axios, { AxiosProgressEvent } from 'axios';
 import { Button, Container, Input } from '@chakra-ui/react';
 import { Progress } from '@chakra-ui/react'
 
-// const FileUpload = () => {
-//   const [file, setFile] = useState<File | null>(null);
-//   const [presignedUrl, setPresignedUrl] = useState<string | null>(null);
-
-//   const getPresignedUrl = async () => {
-//     try {
-//       const response = await axios.get(
-//         'https://zp2dhmgwaa.execute-api.us-east-1.amazonaws.com/generatepresignedurl?fileName=dummydata.txt&contentType=text/plain',
-//         // {
-//         //   params: {
-//         //     fileName: 'dummydata.txt',
-//         //     contentType: 'text/plain',
-//         //   },
-//         // }
-//       );
-//         console.log(response.data.uploadUrl)
-//       setPresignedUrl(response.data.uploadUrl);
-//     } catch (error) {
-//       console.error('Error fetching presigned URL:', error);
-//     }
-//   };
-
-//   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-//     const selectedFile = event.target.files && event.target.files[0];
-//     if (selectedFile) {
-//       setFile(selectedFile);
-//     }
-//   };
-
-//   const uploadFile = async () => {
-//     if (!presignedUrl || !file) {
-//       console.error('Presigned URL or file not available.');
-//       return;
-//     }
-
-//     try {
-//       await axios.put(presignedUrl, file, {
-//         headers: {
-//           'Content-Type': 'text/plain', // Adjust the content type accordingly
-//         },
-//       });
-
-//       console.log('File uploaded successfully!');
-//     } catch (error) {
-//       console.error('Error uploading file:', error);
-//     }
-//   };
-
-//   return (
-//     <Container>
-//       <h1>File Upload</h1>
-//       <Button onClick={getPresignedUrl} colorScheme="teal" mb={4}>
-//         Get Presigned URL
-//       </Button>
-//       <Input type="file" onChange={handleFileChange} mb={4} />
-//       <Button onClick={uploadFile} disabled={!presignedUrl || !file} colorScheme="teal">
-//         Upload File
-//       </Button>
-//     </Container>
-//   );
-// };
-
-// export default FileUpload;
-// ... (your imports remain the same)
-
-
-
-// ... (your imports remain the same)
-
-
-
-// ... (your imports remain the same)
-
-// ... (your imports remain the same)
-
-interface CustomAxiosProgressEvent extends ProgressEvent {
-  lengthComputable: boolean;
-}
-
 const FileUpload = () => {
   const [file, setFile] = useState<File | null>(null);
   const [presignedUrl, setPresignedUrl] = useState<string | null>(null);
-  const [uploadProgress, setUploadProgress] = useState<number>(0);
 
   const getPresignedUrl = async () => {
     try {
       const response = await axios.get(
-        'https://zp2dhmgwaa.execute-api.us-east-1.amazonaws.com/generatepresignedurl?fileName=dummydata.txt&contentType=text/plain'
+        'https://zp2dhmgwaa.execute-api.us-east-1.amazonaws.com/generatepresignedurl?fileName=dummydata.txt&contentType=text/plain',
+        // {
+        //   params: {
+        //     fileName: 'dummydata.txt',
+        //     contentType: 'text/plain',
+        //   },
+        // }
       );
-      console.log(response.data.uploadUrl);
+        console.log(response.data.uploadUrl)
       setPresignedUrl(response.data.uploadUrl);
     } catch (error) {
       console.error('Error fetching presigned URL:', error);
@@ -119,37 +45,14 @@ const FileUpload = () => {
         headers: {
           'Content-Type': 'text/plain', // Adjust the content type accordingly
         },
-        onUploadProgress: (progressEvent: AxiosProgressEvent) => {
-          const customProgressEvent:any = progressEvent as unknown as CustomAxiosProgressEvent;
-          if (customProgressEvent.lengthComputable) {
-            const percentCompleted = Math.round((customProgressEvent.loaded * 100) / customProgressEvent.total!);
-            setUploadProgress(percentCompleted);
-            console.log("value====",customProgressEvent)
-          }
-          
-        }
-        // onUploadProgress: (progressEvent: any) => {
-        //   const customProgressEvent = progressEvent.nativeEvent as CustomAxiosProgressEvent;
-        //   if (customProgressEvent.lengthComputable) {
-        //     const percentCompleted = Math.round((customProgressEvent.loaded * 100) / customProgressEvent.total!);
-        //     setUploadProgress(percentCompleted);
-        //     console.log("value====",customProgressEvent)
-        //   }
-        // }
-        ,
       });
-      then((response: any) => {
-        // Handle the response
-        console.log("response ====>",response)
-      })
-      
+
       console.log('File uploaded successfully!');
-      //setUploadProgress(0); // Reset progress after successful upload
     } catch (error) {
       console.error('Error uploading file:', error);
     }
   };
-console.log( "uploading progress",uploadProgress)
+
   return (
     <Container>
       <h1>File Upload</h1>
@@ -160,8 +63,6 @@ console.log( "uploading progress",uploadProgress)
       <Button onClick={uploadFile} disabled={!presignedUrl || !file} colorScheme="teal">
         Upload File
       </Button>
-      {uploadProgress > 0 && <p>Upload Progress: {uploadProgress}%</p>}
-      <Progress value={uploadProgress} />
     </Container>
   );
 };
@@ -169,14 +70,5 @@ console.log( "uploading progress",uploadProgress)
 export default FileUpload;
 
 
-
-
-
-function then(arg0: (response: any) => void) {
-  throw new Error('Function not implemented.');
-}
-// ... (your other imports remain the same)
-
-// ... (your other imports remain the same)
 
 
